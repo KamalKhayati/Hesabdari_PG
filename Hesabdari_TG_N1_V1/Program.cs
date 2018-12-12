172627ec76fa3bc8ae9383c7677885097de53ad4
kamal khayati <kamal1355@gmail.com>
2018-12-13 03:22:06 +0330

    Add project files.

---
+++ Hesabdari_TG_N1_V1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+
+namespace Hesabdari_TG_N1_V1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BonusSkins.Register();
+            Application.Run(new Form1());
+        }
+    }
+}
